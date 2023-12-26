6f8b92ab5b0660a21452cc5dbd48ab0735c1e2eb
Bria222 <briantechologies@gmail.com>
2023-12-26 20:11:43 +0300

    fix margins and overflow

--- data.js
+++ data.js
@@ -4,11 +4,11 @@ const dataRecords = [
   { code: 3, name: 'Product C', sales: 120, revenue: 600, status: 'Active' },
   { code: 4, name: 'Product D', sales: 80, revenue: 400, status: 'Inactive' },
   { code: 5, name: 'Product E', sales: 200, revenue: 1000, status: 'Active' },
-  { code: 6, name: 'Product F', sales: 90, revenue: 450, status: 'Inactive' },
-  { code: 7, name: 'Product G', sales: 180, revenue: 900, status: 'Active' },
-  { code: 8, name: 'Product H', sales: 110, revenue: 550, status: 'Inactive' },
-  { code: 9, name: 'Product I', sales: 130, revenue: 650, status: 'Active' },
-  { code: 10, name: 'Product J', sales: 160, revenue: 800, status: 'Inactive' },
+  // { code: 6, name: 'Product F', sales: 90, revenue: 450, status: 'Inactive' },
+  // { code: 7, name: 'Product G', sales: 180, revenue: 900, status: 'Active' },
+  // { code: 8, name: 'Product H', sales: 110, revenue: 550, status: 'Inactive' },
+  // { code: 9, name: 'Product I', sales: 130, revenue: 650, status: 'Active' },
+  // { code: 10, name: 'Product J', sales: 160, revenue: 800, status: 'Inactive' },
 ]
 
 export default dataRecords
